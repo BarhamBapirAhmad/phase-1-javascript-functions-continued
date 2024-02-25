f8880416e1538a7a98b13b71d63257e5b82f6bf8
BarhamBapirAhmad <barhamabpirahmed@gmail.com>
2024-02-25 07:02:23 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+ 
+function  wrapAdjective ( cret = "*" ){
+    return function (specification){
+        return `You are ${cret}${specification}${cret}!`
+    }
+    
+}
+wrapAdjective("||")("a dedicated programmer ");
+
+wrapAdjective("*")("a hard worker ");
